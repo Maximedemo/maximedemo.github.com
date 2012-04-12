41ee462304e2c70296665fde54f643bbbda56b61
unknown <Maxime@BadWolf.(none)>
2012-04-12 09:42:46 +0200

    second commit, map 11_04_2012

---
+++ overviewerConfig.js
@@ -0,0 +1,100 @@
+var overviewerConfig = {
+    "worlds": [
+        "world"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                228, 
+                64, 
+                252
+            ], 
+            "last_rendertime": 1334154506, 
+            "name": "Overviewer Render (smooth_lighting)", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 7, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 7, 
+            "path": "world-smooth_lighting"
+        }, 
+        {
+            "spawn": [
+                228, 
+                64, 
+                252
+            ], 
+            "last_rendertime": 1334154506, 
+            "name": "Overviewer Render (smooth_night)", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 7, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 7, 
+            "path": "world-smooth_night"
+        }, 
+        {
+            "spawn": [
+                228, 
+                64, 
+                252
+            ], 
+            "last_rendertime": 1334154506, 
+            "name": "Overviewer Render (cave)", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 7, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 7, 
+            "path": "world-cave"
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "http://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "http://google-maps-icons.googlecode.com/files/home.png", 
+            "compass": "compass_upper-left.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1334216194"
+    }
+};
